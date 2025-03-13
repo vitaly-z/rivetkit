86550a0ca5838ab4cd0c5f3d4229f3031d037d10
NathanFlurry <git@nathanflurry.com>
2025-03-13 07:36:43 +0000

    fix(client): fix fallback priority of websockets & eventsources (#700)

--- packages/actor-core/src/common/eventsource.ts
+++ packages/actor-core/src/common/eventsource.ts
@@ -1,23 +1,28 @@
+import { logger } from "@/client/log";
+
 export async function importEventSource(): Promise<typeof EventSource> {
 	let _EventSource: typeof EventSource;
 
-	// Node.js environment
-	try {
-		const es = await import("eventsource");
-		_EventSource = es.EventSource;
-	} catch (err) {
-		if (typeof EventSource !== "undefined") {
-			// Browser environment
-			_EventSource = EventSource;
-		} else {
+	if (typeof EventSource !== "undefined") {
+		// Browser environment
+		_EventSource = EventSource;
+		logger().debug("using native eventsource");
+	} else {
+		// Node.js environment
+		try {
+			const es = await import("eventsource");
+			_EventSource = es.EventSource;
+			logger().debug("using eventsource from npm");
+		} catch (err) {
 			// EventSource not available
 			_EventSource = class MockEventSource {
 				constructor() {
 					throw new Error(
-						'EventSource support requires installing the "eventsource" package',
+						'EventSource support requires installing the "eventsource" peer dependency.',
 					);
 				}
 			} as unknown as typeof EventSource;
+			logger().debug("using mock eventsource");
 		}
 	}
 
--- packages/actor-core/src/common/websocket.ts
+++ packages/actor-core/src/common/websocket.ts
@@ -1,23 +1,28 @@
+import { logger } from "@/client/log";
+
 export async function importWebSocket(): Promise<typeof WebSocket> {
 	let _WebSocket: typeof WebSocket;
 
-	// Node.js environment
-	try {
-		const ws = await import("ws");
-		_WebSocket = ws.WebSocket as unknown as typeof WebSocket;
-	} catch {
-		if (typeof WebSocket !== "undefined") {
-			// Browser environment
-			_WebSocket = WebSocket;
-		} else {
+	if (typeof WebSocket !== "undefined") {
+		// Browser environment
+		_WebSocket = WebSocket;
+		logger().debug("using native websocket");
+	} else {
+		// Node.js environment
+		try {
+			const ws = await import("ws");
+			_WebSocket = ws.default as unknown as typeof WebSocket;
+			logger().debug("using websocket from npm");
+		} catch {
 			// WS not available
 			_WebSocket = class MockWebSocket {
 				constructor() {
 					throw new Error(
-						'WebSocket support requires installing the "ws" package',
+						'WebSocket support requires installing the "ws" peer dependency.',
 					);
 				}
 			} as unknown as typeof WebSocket;
+			logger().debug("using mock websocket");
 		}
 	}
 
